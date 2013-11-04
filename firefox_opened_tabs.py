f45a1af2b8612fe645b4d784f92238f0fbcf47ab
Stéphane Péchard <stephanepechard@gmail.com>
2013-11-04 16:45:22 +0100

    group URLs in output file as in the FF's groups of tabs

--- firefox_opened_tabs.py
+++ firefox_opened_tabs.py
@@ -9,6 +9,7 @@ from string import Template
 # some constants
 FF_DIR = os.environ['HOME'] + "/.mozilla/firefox/"
 SESSION_FILE = "sessionstore.js"
+FOT_PREFIX = 'FOT_'
 
 if not os.path.isdir(FF_DIR):
     sys.exit("ERROR: can't find your main Firefox directory :-(")
@@ -28,31 +29,50 @@ def find_ff_sessionstore():
 
 def extract_urls(sessionstore):
     """ Parse the sessionstore file to extract urls. """
-    tabs_data = None
-    url_dict = {}
     urls = []
     pinned_urls = []
+    url_dict = {}
+
     with open(sessionstore) as f:
-        tabs_data = json.loads(f.read())
-        windows = tabs_data.get("windows")
-        for window in windows:
-            if 'tabs' in window:
-                print("INFO: you have {} tabs".format((len(window['tabs']))))
-                for tab in window['tabs']:
-                    if 'entries' in tab:
-                        # the last one is the one currently displayed
-                        url = tab['entries'][-1]['url']
-                        if 'pinned' in tab and tab['pinned'] == True:
-                            pinned_urls.append(url)
+        window = json.loads(f.read()).get("windows")[0]
+        # find groups of tabs
+        groups = None
+        if 'extData' in window and 'tabview-group' in window['extData']:
+            groups = json.loads(window['extData']['tabview-group'])
+            for group in groups:
+                url_dict[FOT_PREFIX + groups[group]['title']] = []
+
+        print("INFO: you have {} tabs in {} groups".format(len(window['tabs']),
+                                                           len(groups)))
+
+        if 'tabs' in window:
+            for tab in window['tabs']:
+                if 'entries' in tab:
+                    # the last one in history is the one currently displayed
+                    url = tab['entries'][-1]['url']
+                    if 'pinned' in tab and tab['pinned'] == True:
+                        pinned_urls.append(url)
+                    else:
+                        # is the tab in a group? (FF's Panorama feature)
+                        if 'extData' in tab and 'tabview-tab' in tab['extData']:
+                            tabview = json.loads(tab['extData']['tabview-tab'])
+                            if 'groupID' in tabview:
+                                group_id = str(tabview['groupID'])
+                                group = FOT_PREFIX + groups[group_id]['title']
+                                url_dict[group].append(url)
                         else:
                             urls.append(url)
 
+    # create dictionaries of urls
     if pinned_urls:
         url_dict['pinned'] = pinned_urls
     if urls:
         url_dict['urls'] = urls
 
     return(url_dict)
+
+
+#import ipdb;ipdb.set_trace()
 
 
 def generate_ul(url_list):
@@ -70,11 +90,20 @@ def generate_output(url_dict):
     if 'pinned' in url_dict:
         content += '<h2>Pinned tabs</h2>'
         content += generate_ul(url_dict['pinned'])
+        url_dict.pop('pinned', None)
 
     if 'urls' in url_dict:
         content += '<h2>All other tabs</h2>'
         content += generate_ul(url_dict['urls'])
+        url_dict.pop('urls', None)
 
+    for key in sorted(url_dict.keys()):
+        if url_dict[key]:
+            title = key.replace(FOT_PREFIX, '')
+            if not title:
+                title = 'Unnamed group'
+            content += '<h2>{}</h2>'.format(title)
+            content += generate_ul(url_dict[key])
 
     template = Template("""<!DOCTYPE html>
     <html lang="en"><head>
